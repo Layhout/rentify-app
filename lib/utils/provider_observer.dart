e0325b88f85fec0f493add59e99039d45dc0f86b
Layhout <layhoutworkmail@gmail.com>
2026-03-11 13:48:24 +0700

    chore: imp

--- lib/utils/provider_observer.dart
+++ lib/utils/provider_observer.dart
@@ -4,21 +4,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final class AppObserver extends ProviderObserver {
   @override
   void didAddProvider(ProviderObserverContext context, Object? value) {
-    debugPrint('Provider ${context.provider.name} was initialized with $value');
+    debugPrint('✨ Provider ${context.provider.name} was initialized with $value');
   }
 
   @override
   void didDisposeProvider(ProviderObserverContext context) {
-    debugPrint('Provider ${context.provider.name} was disposed');
+    debugPrint('🗑️ Provider ${context.provider.name} was disposed');
   }
 
   @override
   void didUpdateProvider(ProviderObserverContext context, Object? previousValue, Object? newValue) {
-    debugPrint('Provider ${context.provider.name} updated from $previousValue to $newValue');
+    debugPrint('⚡ Provider ${context.provider.name} updated from $previousValue to $newValue');
   }
 
   @override
   void providerDidFail(ProviderObserverContext context, Object error, StackTrace stackTrace) {
-    debugPrint('Provider ${context.provider.name} threw $error at $stackTrace');
+    debugPrint('💀 Provider ${context.provider.name} threw $error at $stackTrace');
   }
 }
